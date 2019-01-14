06a2e10c7d14496a6490db96749bffbca2b230bf
takkyuuplayer <takkyuuplayer@gmail.com>
2019-01-13 22:30:19 -0500

    Add tests

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 .PHONY: vendor
 
-PHP=$(shell which php)
 COMPOSER=./composer.phar
+DOCKER_COMPOSE=$(shell which docker-compose)
+PHP=$(shell which php)
 
 all: vendor update
 
@@ -11,10 +12,19 @@ vendor: composer.phar
 update: composer/update vendor/update
 
 composer.phar:
-	php -r "readfile('https://getcomposer.org/installer');" | php
+	$(PHP) -r "readfile('https://getcomposer.org/installer');" | $(PHP)
 
 composer/update:
 	$(COMPOSER) self-update
+
+up:
+	$(DOCKER_COMPOSE) up -d
+
+down:
+	$(DOCKER_COMPOSE) down
+
+elasticsearch:
+	curl -H "Content-Type: application/json" -XPOST "localhost:9200/bank/_doc/_bulk?pretty&refresh" --data-binary "@tests/data/accounts.json"
 
 vendor/update: composer.phar
 	$(COMPOSER) update
---
+++ tests/EasticsearchTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class ElasticsearchTest extends TestCase
+{
+    private $client = null;
+    public function setUp()
+    {
+        $this->client = \Elasticsearch\ClientBuilder::create()
+            ->setHosts(
+                [
+                    'elasticsearch',
+                ]
+            )
+            ->build();
+    }
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf(
+            \Elasticsearch\Client::class,
+            $this->client
+        );
+    }
+
+    public function testGet()
+    {
+        $response = $this->client->get(
+            [
+                'index' => 'bank',
+                'type' => '_doc',
+                'id' => '1',
+            ]
+        );
+        $this->assertSame(
+            array(
+                '_index' => 'bank',
+                '_type' => '_doc',
+                '_id' => '1',
+                '_version' => $response['_version'],
+                'found' => true,
+                '_source' =>
+                    array(
+                    'account_number' => 1,
+                    'balance' => 39225,
+                    'firstname' => 'Amber',
+                    'lastname' => 'Duke',
+                    'age' => 32,
+                    'gender' => 'M',
+                    'address' => '880 Holmes Lane',
+                    'employer' => 'Pyrami',
+                    'email' => 'amberduke@pyrami.com',
+                    'city' => 'Brogan',
+                    'state' => 'IL',
+                ),
+            ),
+            $response
+        );
+    }
+
+    public function testSearch()
+    {
+        $response = $this->client->search(
+            [
+                "index" => "bank",
+                "type" => "_doc",
+                "body" => [
+                    "query" => [
+                        "bool" => [
+                            "filter" => [
+                                "range" => [
+                                    "balance" => [
+                                        "gte" => 20000,
+                                        "lte" => 30000,
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        );
+        $this->assertSame(217, $response['hits']['total']);
+    }
+}
